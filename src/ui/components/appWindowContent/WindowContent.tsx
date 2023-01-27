d234a733c7c2203eb9bdaebc5414fde9e6a73fcc
Abbatyya <abbaty48@gmail.com>
2023-01-28 00:30:13 +0100

    -- Added WindowContent Structure -- Refactor themes style in style.css

--- src/ui/components/appWindowContent/WindowContent.tsx
+++ src/ui/components/appWindowContent/WindowContent.tsx
@@ -1,7 +1,20 @@
+import { TabView, TabPanel } from 'primereact/tabview';
 
 export default function WindowContent() {
 
   return (
-    <div id="app-maincontent"></div>
+    <div className={'mt-[4%] mb-[2%] mx-[2%] h-full'}>
+      <TabView>
+        <TabPanel header={'#'} leftIcon='pi pi-home mr-2'>
+          <h1>HOME</h1>
+        </TabPanel>
+        <TabPanel header={'Downloads'} leftIcon='pi pi-download mr-2'>
+          <h1>Download</h1>
+        </TabPanel>
+        <TabPanel header={'About'}>
+          <h1>About</h1>
+        </TabPanel>
+      </TabView>
+    </div>
   );
 }
